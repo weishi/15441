956da4a8d23af9b2895f021b366f6071e30a7806
Wei Shi <weishi@andrew.cmu.edu>
2011-10-12 20:11:55 -0400

    Updated test script with flask

--- runme.sh
+++ runme.sh
@@ -1,2 +1,3 @@
-#make clean && make
-#valgrind --leak-check=full ./lisod 9999 7777 /tmp/lisodLog /tmp/sth /tmp/www /tmp 
+make clean && make
+#valgrind --leak-check=full --track-origins=yes ./lisod 9999 7777 /tmp/lisodLog /tmp/sth /tmp/www /tmp/CGI/cgi_script.py ./weishi.key ./weishi.crt
+valgrind --leak-check=full --track-origins=yes ./lisod 9999 7777 /tmp/lisodLog /tmp/sth /tmp/www /tmp/flask/examples/flaskr/wsgi_wrapper.py ./weishi.key ./weishi.crt
