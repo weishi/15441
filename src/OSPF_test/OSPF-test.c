8c55d632373ae460439ccc2bdc46b859bcfa7e16
Han Liu <hanl1@unix33.andrew.cmu.edu>
2011-11-12 17:35:24 -0500

    updated strucutre of query()/seperated OSPF-test and OSPF/updated Makefile

--- src/OSPF_test/OSPF-test.c
+++ src/OSPF_test/OSPF-test.c
@@ -114,3 +114,39 @@ void query(unsigned targetID, unsigned int* nextHop, int *distance){
   //did not find a route 
   *distance = INF;
 }
+
+int main(){
+  unsigned int* nextHop = malloc_clean(sizeof(unsigned));
+  int* distance = malloc_clean(sizeof(int));
+  int matrix1[4][4] = {{0, 1, 1, 0}, //1
+		       {1, 0, 0, 1}, //3
+		       {1, 0, 0, 1}, //2
+		       {0, 1, 1, 0}};//7 
+  int nodeList1[4] = {1,3,2,7};
+  int numNode = 4;
+  int rootID = 1;
+  updateShortestPath(matrix1[0], numNode, nodeList1, rootID);
+  query(7, nextHop, distance);
+  printf("Matrix 1: Next hop from root to 7 is %d distance %d\n", *nextHop, *distance);
+  
+  int matrix2[4][4] = {{0,1,1,0}, //1
+		       {1,0,1,0}, //4
+		       {1,1,0,1}, //3
+		       {0,0,1,0}};//5
+  int nodeList2[4] = {1,4,3,5};
+  updateShortestPath(matrix2[0], numNode, nodeList2, rootID);
+  query(5, nextHop, distance);
+  printf("Matrix 2: Next hop from root to 5 is %d distance %d\n", *nextHop, *distance);
+
+  int matrix3[4][4] = {{0,1,1,1}, //1
+		       {1,0,1,1}, //4
+		       {1,1,0,1}, //3
+		       {1,1,1,0}};//5
+  int nodeList3[4] = {1,4,3,5};
+  updateShortestPath(matrix3[0], numNode, nodeList3, rootID);
+  query(5, nextHop, distance);
+  printf("Matrix 2: Next hop from root to 5 is %d distance %d\n", *nextHop, *distance);
+  free(distance);
+  free(nextHop);
+  return 0;
+}
--- src/OSPF_test/OSPF.h
+++ src/OSPF_test/OSPF.h
@@ -9,7 +9,6 @@ typedef struct shortestPathState{
   int *previous; 
   int *dist;
   unsigned numNode;
-  unsigned matrixSize; //numNode^2
   unsigned rootID;
   unsigned rootIdx; //Index of the rootNode within the connection matrix
 } shortestPathState;
@@ -27,23 +26,16 @@ unsigned fromID(unsigned nodeID);
   numNode:
    number of nodes in the graph
   nodeList: map from local matrix indeces to actualy node IDs.
-   nodeList[i*col + j] is nodeID for vertex i. So for a 2*2 matrix,
-   nodeList = {id_vertex0, id_vertex0, id_vertex1, id_vertex1}
+   nodeList[i] is nodeID for vertex i.
   rootID= the nodeID of the source for shortest path
-  
-  HAN's CHANGE CHANGES:
-  1. pass in number of nodes instead of matrix size. n*n should be cheaper
-  than sqrt(n)
-  HAN's assumptions:
-  1. nodeList[i*col + j] = nodeID for vertex i. So for a 2*2 matrix,
-  nodeList = {id_vertex0, id_vertex0, id_vertex1, id_vertex1}
-*/
-
+*/  
+ 
 void updateShortestPath(int* matrix, int numNode, unsigned int *nodeList, 
 			unsigned int rootID);
 
 /*
-  Returns the nodeID of the next hop from root to get to node <targetID>
-  following the shortest path. If targetID is root return itself
- */
-unsigned int query(unsigned int targetID);
+  Stores the next hop ID in address pointed by nextHop, and distance to target node
+  in address pointed by distance. In case there does not exist a route, distance will become
+  INF. 
+*/
+void query(unsigned int targetID, unsigned int* nextHop, int* distance);
